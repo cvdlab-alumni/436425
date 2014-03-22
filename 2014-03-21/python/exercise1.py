62a70c31cabd2980f7304981742cd59ba00d202a
Stefano Russo <ste.russo91@gmail.com>
2014-03-22 15:56:33 +0100

    exercise2 - completed

--- 2014-03-21/python/exercise1.py
+++ 2014-03-21/python/exercise1.py
@@ -18,12 +18,12 @@ LOWER TERRACE (floor0)
 floor0_base = PROD([Q(20),Q(8.15)])
 floor0_tiles = PROD([QUOTE([-0.05,19.90,-0.05]),QUOTE([-0.05,8.05,-0.05])])
 
-# duplicating columns: front/rear column rows
+# duplicate columns: front/rear column rows
 column_row1_f0 = STRUCT(NN(3)([ T([1,2])([2-c_size/2,-c_size])(column) , T(1)(8)]))
 column_row2_f0 = STRUCT(NN(2)([ T([1,2])([10-c_size,8.15])(column) , T(1)(8)]))
 columns_f0 = STRUCT([column_row1_f0,column_row2_f0])
 
-# assembling floor0
+# assemble floor0
 floor0 = STRUCT([floor0_base,T(3)(0.001)(COLOR(BROWN)(floor0_tiles)),COLOR(RED)(columns_f0)])
 
 """
@@ -34,7 +34,7 @@ UPPER TERRACE + ROOMS (floor1)
 floor1_base = PROD([Q(28),Q(10.35)])
 floor1_tiles = PROD([QUOTE([-0.05,27.90,-0.05]),QUOTE([-0.05,10.25,-0.05])])
 
-# duplicating columns: front/rear column rows
+# duplicate columns: front/rear column rows
 column_row1_f1 = STRUCT(NN(4)([T([1,2])([2-c_size/2,-c_size])(column),T(1)(8)]))
 column_row2_f1 = STRUCT(NN(2)([column_row1_f1,T(2)(10.35+c_size)]))
 columns_f1 = STRUCT([column_row1_f1,column_row2_f1])
@@ -47,9 +47,9 @@ n_wall = PROD([QUOTE([-8,20]),QUOTE([-10.35+w_width,w_width])])
 w_wall = PROD([QUOTE([-28+w_width,w_width]),Q(10.35)])
 # east wall - door
 e_wall_full = PROD([QUOTE([-8,w_width]),Q(10.35)])
-e_door = PROD([QUOTE([-8,w_width]),QUOTE([-3.7,2.6,-4.07])])
+e_door = PROD([QUOTE([-8,w_width]),QUOTE([-3.7,2.6,-4.05])])
 e_wall = DIFFERENCE([e_wall_full,e_door])
-#assembling external walls
+# assemble external walls
 ext_walls = STRUCT([s_wall,n_wall,w_wall,e_wall])
 
 # INTERNAL WALLS
@@ -58,9 +58,9 @@ w_1_full = PROD([QUOTE([-8-6.5,w_width/2]),QUOTE([-4.4,4.5])])
 w_1_door = PROD([QUOTE([-8-6.5,w_width/2]),QUOTE([-5.7,0.96])])
 w_1 = DIFFERENCE([w_1_full,w_1_door])
 w_1_2 = STRUCT(NN(2)([w_1,T(1)(9)]))
-#wall 3
+# wall 3
 w_3 = PROD([QUOTE([-8-6.5-w_width/2,9]),QUOTE([-4.8,w_width*1.5])])
-#wall 4 and 5 - door
+# wall 4 and 5 - door
 w_4_full = PROD([QUOTE([-8-9.5,w_width/2]),QUOTE([-4.8-w_width*1.5,2.8])])
 w_4_5_full = STRUCT(NN(2)([w_4_full,T(1)(3)]))
 w_4_door = PROD([QUOTE([-8-9.5,w_width/2]),QUOTE([-5.7,0.96])])
@@ -70,13 +70,13 @@ w_6_verts = [ [8+6.5+w_width/2,8],[8+14,8],[8+14,7.7],[8+15.5,7.7],[8+15.5,7.7-w
 	[8+12.5,7.7-w_width/2],[8+12.5,7.8],[8+9.5,7.8],[8+9.5,7.8+w_width/2],[8+6.5+w_width/2,7.8+w_width/2]]
 w_6_cells = [[1,2,9,10],[9,2,8],[8,2,7],[7,2,3],[7,3,6],[3,4,5,6]]
 w_6 = MKPOL([w_6_verts,w_6_cells,None])
-#assembling internal walls
+# assemble internal walls
 int_walls = STRUCT([w_1_2,w_3,w_4_5,w_6])
 
 # wardrobe
 wrdb = PROD([QUOTE([-8-16.5,1]),QUOTE([-0.74,4.45])])
 
-# assembling floor1
+# assemble floor1
 floor1 = STRUCT([floor1_base,T(3)(0.001)(COLOR(BROWN)(floor1_tiles)),COLOR(RED)(columns_f1),
 	T(3)(0.002)(COLOR(CYAN)(ext_walls)),T(3)(0.002)(COLOR(BLUE)(int_walls)),T(3)(0.002)(COLOR(MAGENTA)(wrdb))])
 
@@ -87,11 +87,11 @@ ROOF
 # roof parts
 roof_p1 = PROD([Q(28),Q(10.35)])
 roof_p2 = T([1,2])([-0.1,-0.1])(PROD([Q(28+0.2),Q(10.35+0.2)]))
-# duplicating columns: front/rear column rows
+# duplicate columns: front/rear column rows
 column_row1_r = STRUCT(NN(4)([T([1,2])([2-c_size/2,-c_size])(column),T(1)(8)]))
 column_row2_r = STRUCT(NN(2)([column_row1_r,T(2)(10.35+c_size)]))
 columns_r = STRUCT([column_row1_r,column_row2_r])
-#assembling roof
+# assemble roof
 roof = STRUCT([T(3)(0.001)(roof_p1),COLOR(BROWN)(roof_p2),T(3)(0.001)(COLOR(RED)(columns_r))])
 
 """
--- 2014-03-21/python/exercise2.py
+++ 2014-03-21/python/exercise2.py
@@ -2,13 +2,12 @@ from exercise1 import *
 
 height = 4
 glass_material = [0.1,0.2,0.3,0.5,  0,0,0,0.5,  2,2,2,0.5, 0,0,0,0.5, 100]
+
 """
-SOUTH VIEW
+WINDOWS
 """
 
-wnd_width = 0.3
-wnd_x = 8
-wnd_y = 4
+wnd_width = 0.05
 
 def build_window(width):
 	def build_window_tmp1(x):
@@ -20,18 +19,57 @@ def build_window(width):
 		return build_window_tmp2
 	return build_window_tmp1
 
-window = build_window(0.3)(8)(4)
+"""
+SOUTH VIEW
+"""
 
-# x = PROD([Q(3),Q(6)])
-# x = PROD([x,Q(0.3)])
-# x = MATERIAL([0.1,0.2,0.3,0.5,  0,0,0,0.5,  2,2,2,0.5, 0,0,0,0.5, 100])(x)
+# single windows
+single_big_window = build_window(wnd_width)(4-c_size/2)(height)
+double_big_window = STRUCT( NN(2)([ single_big_window,T(1)(4-c_size/2)]))
+small_window = build_window(wnd_width)(2-c_size/2)(height)
+# assemble all windows
+small_windows = STRUCT(NN(2)([small_window,T(1)(18+c_size/2)]))
+big_windows = STRUCT(NN(2)([ T(1)(2+c_size/2)(double_big_window),T(1)(8) ]))
+windows = STRUCT([small_windows,big_windows])
+# walls
+wall_part = T(1)(2-c_size/2)(PROD([Q(c_size),Q(height)]))
+walls = STRUCT(  NN(3)([ wall_part , T(1)(8)  ])  )
+# assemble south view
+south = STRUCT([COLOR(BLUE)(windows),COLOR(CYAN)(walls)])
+# rotate in vertical plane
+south = R([2,3])(PI/2)(south)
 
-south = STRUCT([window,])
+"""
+NORTH VIEW
+"""
 
+# north view is identical to the south view, but in the opposite direction
+north =  T(1)(20)(R([1,2])(PI)(south))
 
+"""
+EAST VIEW
+"""
 
-# south = R([2,3])(PI/2)(south)
+# build and assemble windows
+east_window_1 = build_window(wnd_width)(4.05)(height)
+east_window_2 = build_window(wnd_width)(3.7)(height)
+east = STRUCT([east_window_1, T(1)(4.05+2.6)(east_window_2)])
+# rotate in vertical plane
+east = R([1,2])(-PI/2)(R([2,3])(PI/2)(COLOR(BLUE)(east)))
 
-VIEW(south)
+"""
+WEST VIEW
+"""
 
-# VIEW(two_and_half_model)
+# build and assemble windows
+west_window = build_window(wnd_width)(3.45)(height)
+west = STRUCT(NN(3)([west_window,T(1)(3.45)]))
+# rotate in vertical plane
+west = R([1,2])(PI/2)(R([2,3])(PI/2)(COLOR(BLUE)(west)))
+
+side_views = STRUCT ([ T([1,2,3])([16-c_size/2,6,10])(south), T([1,2,3])([16-c_size/2,21,10])(north), 
+	T([1,2,3])([6,8.15+10.35+c_size,10])(east), T([1,2,3])([38,8.15+c_size,10])(west)])
+
+mock_up_3D = STRUCT([ two_and_half_model, side_views ])
+
+VIEW(mock_up_3D)
