9decfbddbdb572cc084a76b6dc8b9a7b72deb77c
Stefano Russo <ste.russo91@gmail.com>
2014-03-22 12:04:28 +0100

    exercise1 - complete

--- 2014-03-21/python/exercise1.py
+++ 2014-03-21/python/exercise1.py
@@ -37,11 +37,11 @@ floor1_tiles = PROD([QUOTE([-0.05,27.90,-0.05]),QUOTE([-0.05,10.25,-0.05])])
 
 # duplicating columns: front/rear column rows
 column_row1_f1 = STRUCT(NN(4)([T([1,2])([2-c_size/2,-c_size])(column),T(1)(8)]))
-column_row2_f1 = STRUCT(NN(2)([ column_row1_f1 , T(2)(10.35+c_size)]))
+column_row2_f1 = STRUCT(NN(2)([column_row1_f1,T(2)(10.35+c_size)]))
 columns_f1 = STRUCT([column_row1_f1,column_row2_f1])
 
-# external walls
-w_width = 0.1
+# EXTERNAL WALLS
+w_width = 0.15
 # sud,north,west wall
 s_wall = PROD([QUOTE([-8,20]),Q(w_width)])
 n_wall = PROD([QUOTE([-8,20]),QUOTE([-10.35+w_width,w_width])])
@@ -50,14 +50,40 @@ w_wall = PROD([QUOTE([-28+w_width,w_width]),Q(10.35)])
 e_wall_full = PROD([QUOTE([-8,w_width]),Q(10.35)])
 e_door = PROD([QUOTE([-8,w_width]),QUOTE([-3.7,2.6,-4.07])])
 e_wall = DIFFERENCE([e_wall_full,e_door])
-#assembling walls
+#assembling external walls
 ext_walls = STRUCT([s_wall,n_wall,w_wall,e_wall])
 
+# INTERNAL WALLS
+# walls 1 and 2 - door
+w_1_full = PROD([QUOTE([-8-6.5,w_width/2]),QUOTE([-4.4,4.5])])
+w_1_door = PROD([QUOTE([-8-6.5,w_width/2]),QUOTE([-5.7,0.96])])
+w_1 = DIFFERENCE([w_1_full,w_1_door])
+w_1_2 = STRUCT(NN(2)([w_1,T(1)(9)]))
+#wall 3
+w_3 = PROD([QUOTE([-8-6.5-w_width/2,9]),QUOTE([-4.8,w_width*1.5])])
+#wall 4 and 5 - door
+w_4_full = PROD([QUOTE([-8-9.5,w_width/2]),QUOTE([-4.8-w_width*1.5,2.8])])
+w_4_5_full = STRUCT(NN(2)([w_4_full,T(1)(3)]))
+w_4_door = PROD([QUOTE([-8-9.5,w_width/2]),QUOTE([-5.7,0.96])])
+w_4_5 = DIFFERENCE([w_4_5_full,w_4_door])
+# wall 6
+w_6_verts = [ [8+6.5+w_width/2,8],[8+14,8],[8+14,7.7],[8+15.5,7.7],[8+15.5,7.7-w_width/2],
+	[8+12.5,7.7-w_width/2],[8+12.5,7.8],[8+9.5,7.8],[8+9.5,7.8+w_width/2],[8+6.5+w_width/2,7.8+w_width/2]]
+w_6_cells = [[1,2,9,10],[9,2,8],[8,2,7],[7,2,3],[7,3,6],[3,4,5,6]]
+w_6 = MKPOL([w_6_verts,w_6_cells,None])
+#assembling internal walls
+int_walls = STRUCT([w_1_2,w_3,w_4_5,w_6])
+
+# wardrobe
+wrdb = PROD([QUOTE([-8-16.5,1]),QUOTE([-0.74,4.45])])
+
 # assembling floor1
-floor1 = STRUCT([floor1_base,T(3)(0.001)(COLOR(BROWN)(floor1_tiles)),COLOR(GREEN)(columns_f1),T(3)(0.002)(COLOR(RED)(ext_walls)) ])
+floor1 = STRUCT([floor1_base,T(3)(0.001)(COLOR(BROWN)(floor1_tiles)),COLOR(GREEN)(columns_f1),
+	T(3)(0.002)(COLOR(CYAN)(ext_walls)),T(3)(0.002)(COLOR(BLUE)(int_walls)),T(3)(0.002)(COLOR(MAGENTA)(wrdb))])
 
-VIEW(floor1)
-
+"""
+2.5D MODEL
+"""
 two_and_half_model = STRUCT([floor0,T([1,2,3])([8-c_size/2,8.15+c_size,4])(floor1)])
 
-# VIEW(two_and_half_model)
+VIEW(two_and_half_model)
