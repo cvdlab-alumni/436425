f2d486596c38bc57a456b44e3eb2ccb61a850c53
Stefano Russo <ste.russo91@gmail.com>
2014-03-22 18:38:07 +0100

    exercise4 - incomplete

--- 2014-03-21/python/exercise4.py
+++ 2014-03-21/python/exercise4.py
@@ -1,3 +1,23 @@
 from exercise3 import *
 
+# STEP
+
+p1 = (PROD([Q(0.5),QUOTE([-0.25,0.09])]))
+p2 = (PROD([QUOTE([-0.225,0.04]),QUOTE([-0.16,0.09])]))
+p3_verts = [ [0,0],[0,0.05],[0.5,0.25],[0.5,0.20] ]
+p3 = MKPOL([p3_verts, [[1,2,3,4]], None])
+
+step_2D_partial = STRUCT([p2,p3])
+p1_3D = COLOR(P_DGRAY)(R([2,3])(PI/2)(PROD([p1,Q(4.5)])))
+step_3D_partial = R([2,3])(PI/2)( PROD([step_2D_partial,Q(4.5)]))
+step_3D = STRUCT([p1_3D,step_3D_partial])
+
+# STAIRS
+
+stairs = STRUCT(NN(4)([step_3D,T([1,3])([0.5,0.2])]))
+
+# TODO COMPLETE!!
+
+solid_model_3D = STRUCT([ solid_model_3D, stairs])
+
 VIEW(solid_model_3D)
